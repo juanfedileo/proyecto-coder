9d2704248c481149c0c0770cab5d837adc5d1383
juanfedileo <juanfedileo@gmail.com>
2022-02-22 22:03:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Menu from './Menu';
+import Footer from './Footer';
+import Main from './Main';
+
+function App() {
+    return (
+        <>
+            <Menu />
+            <Main />
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
