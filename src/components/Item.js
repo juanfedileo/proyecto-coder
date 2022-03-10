49a6d716f36a7044e72a4f5d6c91688625eaa198
juanfedileo <juanfedileo@gmail.com>
2022-03-09 21:19:09 -0300

    desafio 6

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
+import { Card } from 'react-bootstrap'
 import ItemCount from './ItemCount'
 
 const Item = (props) => {
     const [stock,setStock] = useState(props.stock)
-  return (
-    <div>
-        <h4>{props.name}</h4>
-        <img src={props.img} alt={props.name} />
-        <h5>{props.cat.join(', ')}</h5>
-        <h6>{'$ '+props.price.toLocaleString('es')}</h6>
+  return (  
+    <Card border="info" style={{ width: '20rem'}}>
+        <Card.Img variant="top" src={props.img} alt={props.name} />
+        <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        
+        <Card.Text>{props.cat.join(', ')}</Card.Text>
+        <Card.Text>{'$ '+props.price.toLocaleString('es')}</Card.Text>
         <ItemCount stock={stock} setStock={setStock}/>
-    </div>
+        </Card.Body>
+    </Card>
   )
 }
 
