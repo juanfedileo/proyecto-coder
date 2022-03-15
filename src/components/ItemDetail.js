1908cea90f95d3ed65ab17550802fb99389bde8c
juanfedileo <juanfedileo@gmail.com>
2022-03-14 21:52:22 -0300

    desafio 7

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -5,14 +5,17 @@ import { Card } from 'react-bootstrap';
 const ItemDetail = (props) => {
 
   const [stock,setStock] = useState(props.product.stock)
+
+  if (props.product.pid == 0)
+    return <></>
   return (  
     <Card border="info" style={{ width: '20rem'}}>
         <Card.Img variant="top" src={props.product.imagen} alt={props.product.name} />
         <Card.Body>
         <Card.Title>{props.product.name}</Card.Title>
         
-        <Card.Text>{props.product.categor}</Card.Text>
-        <Card.Text>{'$ '+props.product.price}</Card.Text>
+        <Card.Text>{props.product.categor.join(', ')}</Card.Text>
+        <Card.Text>{'$ '+props.product.price.toLocaleString('es')}</Card.Text>
         <ItemCount stock={stock} setStock={setStock}/>
         </Card.Body>
     </Card>
